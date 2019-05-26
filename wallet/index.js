7e718ab34e4ae50c3bc3dfee2d1b74a755a23640
Wesley <wesly972@hotmail.com>
2019-05-26 17:32:49 +0200

    wallet create creation

--- wallet/index.js
+++ wallet/index.js
@@ -1,3 +1,4 @@
+const Transaction = require('./transaction');
 const { BALANCE_AT_START } = require('../config');
 const { ec } = require('../util');
 const cryptoHasher = require('../util/crypto-hasher');
@@ -15,6 +16,18 @@ class Wallet {
     sign(data){
         return this.keyPair.sign(cryptoHasher(data))
     }
+
+    // creation d'une transaction et check des soldes
+    createTransaction({ recipient, amount }) {
+        if (amount > this.balance){
+            throw new Error('le montant dépasse le solde du compte');
+
+        }
+        return new Transaction({ senderWallet: this, recipient, amount });
+    }
+
+    
+
 }
 
 module.exports = Wallet;
