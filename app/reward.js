7a0b987fdc315ff9d0442032f37d8def16a61b8d
Wesley <wesly972@hotmail.com>
2019-06-01 22:17:44 +0200

    transaction miner class

---
+++ app/reward.js
@@ -0,0 +1,22 @@
+class TransactionMiner { 
+    constructor({ blockchain, transactionPool, wallet, pubsub })
+    {
+        this.blockchain = blockchain;
+        this.transactionPool = transactionPool;
+        this.wallet = wallet;
+        this.pubsub = pubsub;
+    }
+    mineTransactions() { 
+// recupere les transactions valides du pool
+
+// genere les récompenses des mineurs 
+
+// ajoute un block de ces transactions à la blockchain
+
+// diffuse la blockchain mise à jour
+
+// néttoie la pool 
+    }
+}
+
+module.exports = TransactionMiner;
