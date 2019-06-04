bf7f32e22e373352c17f2bcbb6e91f3bc9a8de99
Wesley <wesly972@hotmail.com>
2019-06-04 10:49:25 +0200

    serve front and ad javascript to frontend

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const request = require('request');
+const path = require('path');
 const Blockchain = require('./blockchain');
 const PubSub = require('./app/pubsub');
 const TransactionPool = require('./wallet/transaction-pool');
@@ -19,6 +20,7 @@ const ROOT_NODE_ADDRESS = `http://localhost:${DEFAULT_PORT}`;
 
 
 app.use(bodyParser.json());
+app.use(express.static(path.join(__dirname,'client')));
 
 app.get('/api/blocks', (req,res) => {
 res.json(blockchain.chain);
@@ -81,6 +83,10 @@ app.get('/api/wallet-info',(req, res) => {
     balance: Wallet.BalanceCalculator({ chain: blockchain.chain, address: wallet.publicKey})
   })
 })
+// 
+app.get('*', (req,res) => { 
+res.sendFile(path.join(__dirname,'./client/index.html'));
+});
 
 // synchronisation lors de la connexion a un node 
 const RootStateSyncher = () => {
