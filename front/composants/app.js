530bf825143f8f236e9c79c60201237fd0841dac
Wesley <wesly972@hotmail.com>
2019-06-04 22:06:07 +0200

    fetch and display wallet info

--- front/composants/app.js
+++ front/composants/app.js
@@ -1,10 +1,23 @@
 import React, { Component } from 'react';
 
 class App  extends Component  {
+    state = { walletInfo: {}};
+
+    componentDidMount(){ 
+       fetch('http://localhost:3000/api/wallet-info').then(response => response.json()).then(json => this.setState({ walletInfo: json }));
+       
+
+    }
+
+
+
     render() { 
+        const { address, balance } = this.state.walletInfo;
         return ( 
            <div> 
                Bienvenue sur la blockchain de SupBank
+               <div> addresse du compte : {address}</div>
+               <div> Solde du compte : {balance} </div>
            </div> 
         );
     }
