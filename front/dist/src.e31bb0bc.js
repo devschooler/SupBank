a39abfda9ba5b1e1aede4e026a78e46fa62a6f76
Wesley <wesly972@hotmail.com>
2019-06-04 23:57:20 +0200

    seed the backend with data

--- front/dist/src.e31bb0bc.js
+++ front/dist/src.e31bb0bc.js
@@ -23296,7 +23296,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "63629" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "60533" + '/');
 
   ws.onmessage = function (event) {
     var data = JSON.parse(event.data);
